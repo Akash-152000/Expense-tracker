826ff9de70e568ba10d9309cf20e8eaefc5022e2
Akash-152000 <akash152000akash@gmail.com>
2025-07-14 18:43:08 +0530

    Initial commit: setup Next.js + Tailwind + ESLint + Prettier

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Expense Tracker</h1>
+    </main>
+  );
+}
